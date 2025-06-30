ada10b112e4625102f2eced904ad1d7cf3afe5c8
Rebecaa1227 <167133017+Rebecaa1227@users.noreply.github.com>
2025-06-30 21:04:32 +0000

    up

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,25 +9,23 @@ export default defineConfig({
   themeConfig: {
     nav: [
       { text: '1. Introducao', link: '/1_0_introducao' },
-      { text: '1.1. Objetivo', link: '/1_1objetivos' },
-      { text: '1.2. Escopo', link: '/1_2_Escopo' },
-      { text: '1.3. Definições', link: '/1_3_introducao' },
-      { text: '1.4. Publico Alvo', link: '/1_4_introducao' },
-      { text: '2. Descrição', link: '/2_0_introducao' },
-      { text: '3. Recursos', link: '/3_0_Recursos e Funcionalidades' },
-      { text: '4. Caso de Uso', link: '/4_1_Casos de Usos' },
-      { text: '8. Riscos', link: '/8_Riscos e mitigação' },
-      { text: '9. Custos', link: '/9_Custos e Orçamento' },
-      { text: '10. Considerações Finais', link: '/10_Considerações' },
-
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     { text: 'Item A', link: '/item-1' },
-      //     { text: 'Item B', link: '/item-2' },
-      //     { text: 'Item C', link: '/item-3' },
-      //   ],
-      // },
+  
+       {
+         text: 'Dropdown Menu',
+         items: [
+          { text: '1. Introducao', link: '/1_0_introducao' },
+          { text: '1.1. Objetivo', link: '/1_1objetivos' },
+          { text: '1.2. Escopo', link: '/1_2_Escopo' },
+          { text: '1.3. Definições', link: '/1_3_introducao' },
+          { text: '1.4. Publico Alvo', link: '/1_4_introducao' },
+          { text: '2. Descrição', link: '/2_0_introducao' },
+          { text: '3. Recursos', link: '/3_0_Recursos e Funcionalidades' },
+          { text: '4. Caso de Uso', link: '/4_1_Casos de Usos' },
+          { text: '8. Riscos', link: '/8_Riscos e mitigação' },
+          { text: '9. Custos', link: '/9_Custos e Orçamento' },
+          { text: '10. Considerações Finais', link: '/10_Considerações' },
+         ],
+       },
 
       // ...
     ],
