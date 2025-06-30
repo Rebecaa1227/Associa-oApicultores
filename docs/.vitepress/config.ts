56e48353db4a0db243a5f18f32290c1ec03bca26
Rebecaa1227 <167133017+Rebecaa1227@users.noreply.github.com>
2025-06-30 21:10:59 +0000

    up

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
          text: 'Menu',
          items: [
           { text: '1. Introducao', link: '/1_0_Intoducao' },
-          { text: '1.1. Objetivo', link: '/1_1_objetivos' },
+          { text: '1.1. Objetivo', link: '/1_1_objetivo' },
           { text: '1.2. Escopo', link: '/1_2_Escopo' },
           { text: '1.3. Definições', link: '/1_3_Definiçoes, Acronimos e abreviações' },
           { text: '1.4. Publico Alvo', link: '/1_4_publico alvo' },
